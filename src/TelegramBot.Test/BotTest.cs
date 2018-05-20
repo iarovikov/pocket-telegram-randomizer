25713b7f0949209970600321e0339e5997972c8e
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2018-05-20 21:50:02 +0200

    Initial commit

---
+++ src/TelegramBot.Test/BotTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace TelegramBot.Test
+{
+    public class BotTest
+    {
+        [Fact]
+        public async void PingTest()
+        {
+            var sut = new Bot();
+            var result = await sut.TestApiAsync();
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async void GetUpdatesTest()
+        {
+            var sut = new Bot();
+            var result = await sut.GetUpdatesAsync();
+            
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ src/TelegramBot/Bot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public class Bot
+    {
+        private TelegramBotClient BotClient { get; }
+        
+        public Bot()
+        {
+            BotClient = new TelegramBotClient("api key");
+        }
+        
+        public async Task<string> TestApiAsync()
+        {
+            var me = await BotClient.GetMeAsync();
+            return $"Hello! My name is {me.FirstName}";
+        }
+
+        public async Task<Update[]> GetUpdatesAsync()
+        {
+            var result = await BotClient.GetUpdatesAsync();
+            return result;
+        }
+    }
+}
+
